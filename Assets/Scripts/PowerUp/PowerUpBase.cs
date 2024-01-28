fac70759a6565430e2a4ef6207cf6a5da9e5bafd
JefersonSerafina <jeferson.serafina@gmail.com>
2024-01-28 14:15:33 -0300

    exercicio21
    powerUpCoins was deleted. couldnt make it work

--- Assets/Scripts/PowerUp/PowerUpBase.cs
+++ Assets/Scripts/PowerUp/PowerUpBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpBase : ItemCollectableBase
+public class PowerUpBase : ItemCollactableBase
 {
     [Header("Power Up")]
     public float duration;
