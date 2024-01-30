542585818f0860009618cc292a496ef6847bdd36
JefersonSerafina <jeferson.serafina@gmail.com>
2024-01-30 09:18:47 -0300

    Commit Dúvida Tutor

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -8,13 +8,21 @@ public class LevelManager : MonoBehaviour
 
     public List<GameObject> levels;
 
+    public List<LevelPieceBaseSetup> levelPieceBaseSetups;
+
     [SerializeField] private int _index;
     private GameObject _currentLevel;
-    
+
+    public ArtManager.ArtType ArtType;
+
+
+    private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
+    private LevelPieceBaseSetup _currSetup;
 
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -39,6 +47,75 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+
+    #region PIECES
+    
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+        CleanSpawnedPieces();
+
+        if (_currSetup != null) 
+        {
+            _index++;
+
+            if (_index >= levelPieceBaseSetups.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currSetup = levelPieceBaseSetups[_index];
+
+        for (int i = 0; i < _currSetup.piecesStartNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPiecesStart);
+        }
+        for (int i = 0; i < _currSetup.piecesNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPieces);
+        }
+        for (int i = 0; i < _currSetup.piecesEndNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPiecesEnd);
+        }
+
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var  lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+
+        foreach (var p in spawnedPiece.GetComponentsInChildren<ArtPiece>())
+        {
+            p.ChangePiece(ArtManager.Instance.GetSetupByType(_currSetup.artType).gameObject);
+        }
+
+
+        _spawnedPieces.Add(spawnedPiece);
+
+    }
+
+    private void CleanSpawnedPieces()
+    {
+        for(int i = _spawnedPieces.Count - 1; i >= 0; i--)
+        {
+            Destroy(_spawnedPieces[i].gameObject);
+        }
+
+        _spawnedPieces.Clear();
+    }
+
+    #endregion
 
     private void Update()
     {
